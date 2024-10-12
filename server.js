a24726f8a1af956073aff5c253f758093563e741
Trong Truong <tro@sucafina.com>
2024-10-12 13:07:02 +0700

    The necessary folders and packages when initializing a project

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./src/app');
+const port = 3055;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// process.on('SIGINT', (err) => {
+//   server.close(() => {
+//     console.log('Exit Server Process');
+//   });
+// });
